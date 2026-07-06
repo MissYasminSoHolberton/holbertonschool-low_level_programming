5b3e0522c4a549e8c0ffa29353e98e7e7f4d4288
Yasmin So School <14821@holbertonstudents.com>
2026-07-06 18:53:25 +1000

    feat: adding 6-abs.c

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - output the absolute of number
+ * @n: number to convert
+ *
+ * Return: always 0 (success)
+ */
+int _abs(int n)
+{
+if (n >= 0)
+{
+return (n);
+}
+return (n * -1);
+}
