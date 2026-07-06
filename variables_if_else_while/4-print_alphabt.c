bdf5711cbc2cd8f10b28834a0f698850aa29ef94
Yasmin So School <14821@holbertonstudents.com>
2026-07-06 15:29:52 +1000

    fix: adding the correct file

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - prints all the lowercase alphabet
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+
+int i;
+
+for (i = 97; i <= 122; i++)
+{
+if (i == 101 || i == 113)
+{
+continue;
+}
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
