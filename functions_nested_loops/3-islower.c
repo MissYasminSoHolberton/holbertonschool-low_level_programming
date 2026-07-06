4bcde8fee1366edc7493ba1dbb3fdb46909a241a
Yasmin So School <14821@holbertonstudents.com>
2026-07-06 18:04:58 +1000

    feat: adding 3-islower.c

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _islower - prints 1 if char is lowercase otherwise 0
+ * @c: character to check
+ *
+ * Return: if char is lowercase return 1 otherwise retutn 0
+ */
+int _islower(int c)
+{
+if (c >= 97 && c <= 122)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
