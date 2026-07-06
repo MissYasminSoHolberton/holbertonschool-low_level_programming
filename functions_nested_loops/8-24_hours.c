5579e9d5e68916b043a52ff082946158595319a2
Yasmin So School <14821@holbertonstudents.com>
2026-07-06 20:02:25 +1000

    fix: unprinted blocks of time

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -7,27 +7,19 @@
  */
 void jack_bauer(void)
 {
-int hour1;
-int hour10;
-int min1;
-int min10;
+int hour;
+int min;
 
-for (hour10 = 0; hour10 <= 2; hour10++)
+for (hour = 0; hour <= 24; hour++)
 {
-for (hour1 = 0; hour1 <= 4; hour1++)
+for (min = 0; min <= 59; min++)
 {
-for (min10 = 0; min10 <= 5; min10++)
-{
-for (min1 = 0; min1 <= 9; min1++)
-{
-_putchar(hour10 + '0');
-_putchar(hour1 + '0');
+_putchar((hour / 10) + '0');
+_putchar((hour % 10) + '0');
 _putchar(':');
-_putchar(min10 + '0');
-_putchar(min1 + '0');
+_putchar((min / 10) + '0');
+_putchar((min % 10) + '0');
 _putchar('\n');
-}
-}
 }
 }
 }
