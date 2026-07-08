255d905de652748055d29a1345d2a06b1f55fa2b
Yasmin So School <14821@holbertonstudents.com>
2026-07-08 17:08:41 +1000

    feat: adding 7-print_diagonal.c

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * print_diagonal - print diagonal line with width and height of n
+ * @n: width and height of diagonal line
+ */
+
+void print_diagonal(int n)
+{
+	int i;
+	int j;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	for (i = 1; i <= n; i++)
+	{
+		if (n <= 0)
+		{
+			_putchar('0');
+			break;
+		}
+
+		for (j = 1; j <= n; j++)
+		{
+			if (j == i)
+			{
+				_putchar('\\');
+			}
+			else
+			{
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
