b489aefe3ae50989df38edc25f8eedf220f3f71e
Yasmin So School <14821@holbertonstudents.com>
2026-07-08 12:19:37 +1000

    feat: adding 0-isupper.c

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isupper - checks if char is is upper case
+ * @c: input char
+ * 
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
