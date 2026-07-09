16286f7cc96bd5e8067f7da725fc76a177c11a8a
Yasmin So School <14821@holbertonstudents.com>
2026-07-09 14:58:39 +1000

    feat: output based on conditions

--- simple-calculator/calculator.c
+++ simple-calculator/calculator.c
@@ -9,20 +9,28 @@ int main(void)
 {
 	int n;
 
-	printf("Simple Calculator\n");
+	printf("Welcome to my Simple Calculator\n\n");
+	printf("Options:\n");
 	printf("1) Add\n");
 	printf("2) Subtract\n");
 	printf("3) Multiply\n");
 	printf("4) Divide\n");
-	printf("0) Quit\n");
+	printf("0) Quit\n\n");
 
 	printf("Choice: ");
 	scanf("%d", &n);
 
-	if (n == 0)
+	if (n > 0 && n < 5)
 	{
-		printf("Bye!\n");
-		return (0);
+		printf("Your Choice: %d\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("\nBye!\n");
+	}
+	else
+	{
+		printf("Incorrect option :(\n");
 	}
 	return (0);
 }
