4758f2fd03eadd74207f123e5f992314fc78bbaa
Yasmin So School <14821@holbertonstudents.com>
2026-07-13 20:01:50 +1000

    feat: added 4-print_rev.c

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_rev - print string in reverse
+ * @s: string to print in reverse
+ */
+void print_rev(char *s)
+{
+	int length;
+
+	length = 0;
+
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+
+	while (length >= 0)
+	{
+		_putchar(s[length]);
+		length --;
+	}
+	_putchar('\n');
+}
