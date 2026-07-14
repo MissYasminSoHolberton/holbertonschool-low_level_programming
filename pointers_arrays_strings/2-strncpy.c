3c4a792278c9bbc8efb9427e3e8c25718fb56303
Yasmin So School <14821@holbertonstudents.com>
2026-07-14 14:59:02 +1000

    feat: added 2-strncpy.c

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * get_len - get length of the string
+ * @str: string input
+ *
+ * Return: length of the string
+ */
+int get_len(char *str)
+{
+	int len;
+
+	len = 0;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	return (len);
+}
+
+/**
+ * _strncpy - replaces the elements of string
+ * @src: first string
+ * @dest: second string and the output of the concatination
+ * @n: end of copy
+ *
+ * Return: output string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int src_len;
+	int dest_len;
+	int i;
+
+	src_len = get_len(src);
+	dest_len = get_len(dest);
+	i = 0;
+
+	while (i < n && i <= dest_len && i <= src_len)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
