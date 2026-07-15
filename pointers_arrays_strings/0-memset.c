75d0a955961ee62a40c84c06574044f3d8e131ab
Yasmin So School <14821@holbertonstudents.com>
2026-07-15 19:35:31 +1000

    fix: wrong return address

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -11,8 +11,10 @@
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
+	char *start_of_s;
 
 	i = 0;
+	start_of_s = s;
 
 	while (i < n)
 	{
@@ -20,5 +22,5 @@ char *_memset(char *s, char b, unsigned int n)
 		s++;
 		i++;
 	}
-	return (s);
+	return (start_of_s);
 }
