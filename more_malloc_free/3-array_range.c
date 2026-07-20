0f782952448a02052a105d406e7b73710549fa23
Yasmin So School <14821@holbertonstudents.com>
2026-07-20 18:16:41 +1000

    feat: adding int *array_range(int min, int max);

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+
+/**
+ * array_range - allocates memory that is the size of the range
+ * @min: low range of the array
+ * @max: high range of the array
+ *
+ * Return: address to the allocated memory
+ */
+int *array_range(int min, int max)
+{
+	int *result;
+	unsigned int i;
+	unsigned int size;
+
+	if (min > max)
+	{
+		return (NULL);
+	}
+
+	size = max - min + 1;
+
+	result = malloc(sizeof(int) * size);
+
+	if (result == NULL)
+	{
+		return (NULL);
+	}
+
+	i = 0;
+	while (i <= size)
+	{
+		result[i] = min + i;
+		i++;
+	}
+
+	return (result);
+}
