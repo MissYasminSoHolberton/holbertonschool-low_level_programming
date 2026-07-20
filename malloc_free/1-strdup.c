e3eff34ba0f6d7cefa3b939e7385bfa8973f6c44
Yasmin So School <14821@holbertonstudents.com>
2026-07-20 11:09:14 +1000

    feat: added 1-strdup.c

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+
+/**
+ * strdup - copies a string to a new string
+ * @str: string to copy
+ *
+ * Return: pointer to the created string or NULL if unsuccessful
+ */
+char *_strdup(char *str)
+{
+	char *char_array;
+	int i;
+	int size;
+
+	size = sizeof(str) + 1;
+	char_array = malloc(size);
+
+	if (char_array == NULL || size == 0)
+	{
+		return (NULL);
+	}
+
+	i = 0;
+	while (i < size)
+	{
+		char_array[i] = str[i];
+		i++;
+	}
+
+	return (char_array);
+}
