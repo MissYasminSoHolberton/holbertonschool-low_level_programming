ec0d2489784ba438023ef4864d6b5c9f4ac74ba9
Yasmin So School <14821@holbertonstudents.com>
2026-07-20 17:48:25 +1000

    feat: added 2-calloc.c

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+
+/**
+ * _calloc - allocates memory with contents set as 0
+ * @nmemb: number or members
+ * @size: size of each member
+ *
+ * Return: address to the allocated memory
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *result;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+
+	result = malloc(nmemb * size);
+
+	if (result == NULL)
+	{
+		return (NULL);
+	}
+
+	i = 0;
+	while (i < nmemb)
+	{
+		((char *)result)[i] = 0;
+		i++;
+	}
+
+	return (result);
+}
