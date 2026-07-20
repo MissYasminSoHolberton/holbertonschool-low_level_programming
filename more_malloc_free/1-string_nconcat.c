3e94a73f68c4ebc09a9f9751ec55277ba65d5541
Yasmin So School <14821@holbertonstudents.com>
2026-07-20 16:54:26 +1000

    feat: added 1-string_nconcat.c

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,70 @@
+#include <stdlib.h>
+
+/**
+ * _strlen - gets the length of a string
+ * @str:string
+ *
+ * Return: length of string
+ */
+int _strlen(char *str)
+{
+	int len;
+	int i;
+
+	len = 0;
+	i = 0;
+
+	while (str[i] != '\0')
+	{
+		len++;
+		i++;
+	}
+	return (len);
+}
+
+/**
+ * string_nconcat - concatinates two strings
+ * @s1: string 1
+ * @s2: string 2
+ * @n: number of bytes of the second string to add
+ *
+ * Return: pointer to the created string or NULL if unsuccessful
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *char_array;
+	int i;
+	unsigned int j;
+	int size;
+
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
+	i = 0;
+	size = _strlen(s1) + _strlen(s2);
+
+	char_array = malloc(size + 1);
+
+	if (char_array == NULL)
+		return (NULL);
+
+	i = 0;
+	while (*s1 != '\0')
+	{
+		char_array[i] = *s1;
+		s1++;
+		i++;
+	}
+	j = 0;
+	while (*s2 != '\0' && j < n)
+	{
+		char_array[i] = *s2;
+		s2++;
+		i++;
+		j++;
+	}
+	return (char_array);
+}
