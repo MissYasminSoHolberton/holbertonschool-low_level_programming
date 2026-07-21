a2fd509e1b4a9a017006f51e9edca87547e7291d
Yasmin So <yasmin@yasmin.so>
2026-07-21 15:59:24 +1000

    feat: adding 1-init_dog.c

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+
+/**
+ * init_dog - initalize dog struct
+ * @d: dog struct
+ * @name: name of doggo
+ * @age: age of doggo
+ * @owner: owner of doggo
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
