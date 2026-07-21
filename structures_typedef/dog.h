a914cb06fee776e1e8746ce3770127e2bf340b3a
Yasmin So School <14821@holbertonstudents.com>
2026-07-21 15:20:20 +1000

    feat: adding dog.h

---
+++ structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - information about poopy!!!
+ * @name: name of poppy
+ * @age: age of the good poppy
+ * @owner: feeder of poopy
+ *
+ * Description: Dog has a name, age and owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
