44e76a8602ee074ebee38eae8286d24c8fd7a146
Yasmin So <yasmin@yasmin.so>
2026-07-21 16:25:56 +1000

    fix: null edge case

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stddef.h>
 
 /**
  * init_dog - initalize dog struct
@@ -9,6 +10,10 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (d == NULL)
+	{
+		return;
+	}
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
