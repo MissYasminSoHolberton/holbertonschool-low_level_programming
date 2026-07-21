7f1c53b5a43bffda4fe4762e005e1cff58fa4910
Yasmin So <yasmin@yasmin.so>
2026-07-21 19:41:47 +1000

    feat: adding 5-free_dog.c

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+#include <stddef.h>
+#include <stdlib.h>
+
+/**
+ * free_dog - free the doggo! free the memory allocated
+ * @d - struct that was made
+ */
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
