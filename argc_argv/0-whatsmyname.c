7faff7ea96b56ce4967e679ba3ec6ca7284b9dd2
Yasmin So <yasmin@yasmin.so>
2026-07-28 15:15:42 +1000

    feat: adding 0-whatsmyname.c

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of program
+ * @argc: number of arguments
+ * @argv: array or args
+ *
+ * Return: 0 if sucess
+ */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
