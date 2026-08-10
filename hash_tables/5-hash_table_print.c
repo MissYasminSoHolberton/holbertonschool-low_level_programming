75ce49ad39defcd3226f87b6c5fa56318feb4caf
Yasmin So <yasmin@yasmin.so>
2026-08-10 19:50:53 +1000

    fix: silly me did not push the file

---
+++ hash_tables/5-hash_table_print.c
@@ -0,0 +1,33 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - print hastable
+ * @ht: hashtable to edit
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	long unsigned int i;
+	hash_node_t *node;
+	char *delimeter;
+
+	if (ht == NULL)
+		return;
+
+	printf("{");
+	i = 0;
+	delimeter = "";
+	while (i < ht->size)
+	{
+		node = ht->array[i];
+		while (node != NULL)
+		{
+			printf("%s", delimeter);
+			printf("'%s': ", node->key);
+			printf("'%s'", node->value);
+			node = node->next;
+			delimeter = ", ";
+		}
+		i = i + 1;
+	}
+	printf("}\n");
+}
