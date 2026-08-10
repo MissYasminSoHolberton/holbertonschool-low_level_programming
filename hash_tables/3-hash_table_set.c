6ea3f615b181ec2dbc8019c9c64c54533e6873c1
Yasmin So <yasmin@yasmin.so>
2026-08-10 19:04:07 +1000

    style: added space

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -44,7 +44,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	index = key_index((unsigned char *)key, ht->size);
 
 	node = ht->array[index];
-	new = ht_pair(key,value);
+	new = ht_pair(key, value);
 
 	if (node == NULL)
 	{
