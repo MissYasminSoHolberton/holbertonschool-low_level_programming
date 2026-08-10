30aeec6ec0142f66476800a3473acf246de4c2c6
Yasmin So <yasmin@yasmin.so>
2026-08-10 18:48:27 +1000

    feat: adding 3-hash_table_set.c

---
+++ hash_tables/3-hash_table_set.c
@@ -0,0 +1,58 @@
+#include "hash_tables.h"
+
+/**
+ * ht_pair - creates a node
+ * @key: key
+ * @value: value
+ *
+ * Return: created pair
+ */
+hash_node_t *ht_pair(const char *key, const char *value)
+{
+	hash_node_t *node = malloc(sizeof(hash_node_t));
+
+	node->key = malloc(strlen(key) + 1);
+	node->value = malloc(strlen(value) + 1);
+
+	strcpy(node->key, key);
+	strcpy(node->value, value);
+
+	node->next = NULL;
+
+	return (node);
+}
+
+/**
+ * hash_table_set - adds element to hashtable
+ * @key: key
+ * @value: value to be added
+ * @ht: hashtable to edit
+ *
+ * Return: 1 succeded, 0 otherwise
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	int index;
+	hash_node_t *node;
+	hash_node_t *curr;
+
+	index = key_index((unsigned char *)key, ht->size);
+
+	node = ht->array[index];
+
+	if (node == NULL)
+	{
+		ht->array[index] = ht_pair(key, value);
+		return (1);
+	}
+
+	curr = node;
+	while (curr->next != NULL)
+	{
+		curr = curr->next;
+	}
+
+	curr->next = ht_pair(key, value);
+
+	return (1);
+}
