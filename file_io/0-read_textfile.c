e3caef50c2f543afe18dafac326861783f1b6d41
yasmin@yasmin.so <yasmin@yasmin.so>
2026-08-18 22:52:20 +1000

    fix: edge case

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -19,23 +19,27 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	}
 
-	if (filename == NULL)
+	if (filename == NULL || *filename == '\0')
 	{
 		return (0);
 	}
 
 	fd = open(filename, O_RDONLY);
 
-	if (fd == 0)
+	if (fd == -1)
 	{
+		free(c);
 		return (0);
 	}
 
 	sz = read(fd, c, letters);
-
-	write(STDOUT_FILENO, c, letters);
+	if (sz > 0)
+	{
+		write(STDOUT_FILENO, c, sz);
+	}
 
 	close(fd);
+	free(c);
 
 	return (sz);
 }
